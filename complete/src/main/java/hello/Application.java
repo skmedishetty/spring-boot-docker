b9811428aa5e8208bb9517f47d15dd9011ddd81c
skmedishetty <sanjeev.medishetty@gmail.com>
2021-11-25 21:51:44 -0500

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Jitendra !!";
+        return "Hello Mani !!";
     }
 
     public static void main(String[] args) {
